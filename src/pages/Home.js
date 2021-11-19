4138457a9c405148a2e804f341b8a63d6847996f
iago <iago.pferreiravr@gmail.com>
2021-11-19 08:45:10 -0300

    feat: Rendering component Home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CharacterList from '../components/CharacterList';
+
+class Home extends Component {
+  render() {
+    return (
+      <main>
+        <CharacterList />
+      </main>
+    );
+  }
+}
+
+export default Home;
