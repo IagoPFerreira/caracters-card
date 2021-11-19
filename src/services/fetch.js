f73aaa04dd9201d5672eddb2877012464d3db360
iago <iago.pferreiravr@gmail.com>
2021-11-19 08:40:32 -0300

    feat: Implementing fetch by id

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,13 +1,22 @@
-import heroes from "../data/heroes";
+import characters from "../data/characters";
 
 const randomTime = () => {
-  return Math.round(Math.random() * 1000)
+  return Math.round(Math.random() * 2500)
 }
 
 const fetcher = new Promise ((resolve, reject) => {
   const error = "ERROR 404. Not found!"
   return setTimeout(() => {
-    true ? resolve(heroes) : reject(error)
+    true ? resolve(characters) : reject(error)
+  }, randomTime());
+});
+
+export const fetcherId = (id) => new Promise ((resolve, reject) => {
+  const error = "ERROR 404. Not found!"
+  return setTimeout(() => {
+    const character = characters.find((hero) => hero.id === id)
+
+    true ? resolve(character) : reject(error)
   }, randomTime());
 });
 
